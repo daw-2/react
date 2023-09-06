1536ccf0255380c774dcec7194091ba4d6951190
Matthieu Mota <matthieumota@gmail.com>
2023-09-06 14:05:18 +0200

    Init project

---
+++ 03-todos/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <input type="text" />
+        <span role="button">OK</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
