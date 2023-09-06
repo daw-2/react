b2ef62d89baf2bafb68446135bedf300133f8d50
Matthieu Mota <matthieumota@gmail.com>
2023-09-06 14:16:22 +0200

    Begin header

---
+++ 03-todos/src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h1>Todo List 1 (3 todos)</h1>
+  );
+}
+
+export default Header;
--- 03-todos/src/pages/Home.jsx
+++ 03-todos/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import Header from '../components/Header';
+
 function Home() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: 'Faire les courses', done: false },
+    { id: 2, name: 'Travailler', done: true },
+    { id: 3, name: 'Surveiller Fiorella', done: true }
+  ]);
+
   return (
     <>
-      HEADER
+      <Header />
       FORM
       LIST
         TODO
